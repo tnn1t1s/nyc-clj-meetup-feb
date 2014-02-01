b0dbc87fd3a74c8774b5c9e1b497cc2142b2dd55
tinn1t1s <David@davids-macbook.home>
2014-02-01 17:12:42 -0500

    cleanup duo

--- src/nyc_clj_meetup_feb/duo.clj
+++ src/nyc_clj_meetup_feb/duo.clj
@@ -1,5 +1,6 @@
 (ns nyc-clj-meetup-feb.duo
-  (:use overtone.live overtone.studio.scope))
+  (:use overtone.live
+        overtone.studio.scope))
 
 
 ; intro to oscilloscope and spectrogram
@@ -24,45 +25,47 @@
 (defonce early-g (group "early birds" :head main-g))
 (defonce later-g (group "latecomers" :after early-g))
 
-(defsynth modulated-freq-tri [
+(ctl mf3 :freq-amp 7.0)
+
+(defsynth monotron-duo [
   freq-bus 0 
   cutoff-bus 0
   rez-bus 0
   freq 220
   freq-amp 55
   cutoff 440
-  mult 1.0
+  cutoff-amp 1.0
   rez 1.0
   rez-amp 1.0
   ]
   (let [
-  freq freq
+  cutoff (+ cutoff (* cutoff-amp (in:kr cutoff-bus)))
+  freq (+ freq (* freq-amp (in:kr freq-bus)))
   rez (+ rez (* rez-amp (in:kr rez-bus)))
   ]
-    (out 0 (pan2 (rlpf (saw freq) (+ cutoff (* mult (in:kr cutoff-bus))) rez)))))
-
-; (out 0 (pan2 (rlpf (lf-tri freq) (+ cutoff (in:kr cutoff-bus)) 1.0)))))
+    (out 0 (pan2 (rlpf (saw freq) cutoff rez)))))
 
 (comment
 (do
 (def sin-synth-inst (sin-synth [:tail early-g] sin-bus))
 (def mod-synth-inst (mod-synth [:tail early-g] mod-bus))
 (def lfo-synth-inst (mod-synth [:tail early-g] lfo-bus))
-(def mft (modulated-freq-tri [:tail later-g] sin-bus mod-bus lfo-bus 439))
-(def mf2 (modulated-freq-tri [:tail later-g] sin-bus mod-bus lfo-bus 440))
-(def mf3 (modulated-freq-tri [:tail later-g] sin-bus mod-bus lfo-bus 441))
+(def mft (monotron-duo [:tail later-g] sin-bus mod-bus lfo-bus 439))
+(def mf2 (monotron-duo [:tail later-g] sin-bus mod-bus lfo-bus 440))
+(def mf3 (monotron-duo [:tail later-g] sin-bus mod-bus lfo-bus 441))
 ))
 
 
-(stop)
-(stop)
 (comment
-(ctl mod-synth-inst :freq 5)
-(ctl mft :mult 100.0)
-(ctl mft :rez 0.05)
-(ctl mf2 :rez 0.05)
-(ctl mft :freq 439)
-(ctl mf3 :freq 119)
+(ctl mod-synth-inst :freq 1)
+(ctl mft :cutoff-amp 400.0)
+(ctl mft :rez 0.01)
+(ctl mf2 :rez 0.01)
+(ctl mf3 :rez 0.01)
+(ctl mft :freq 440)
+(ctl mf2 :freq 439)
+;(ctl mf3 :freq 1760)
+(ctl mf3 :freq 1760)
 (ctl mf2 :cutoff 300)
 )
 
